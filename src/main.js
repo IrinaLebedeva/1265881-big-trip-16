264026cc4f12135920ef47b865e164677887fe58
irina.lebedeva <irina.lebedeva@travelline.ru>
2021-11-25 22:28:51 +0300

    Добавляет модуль фитров

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import {renderTemplate, RenderPosition} from './render.js';
 import {createHeaderMenuTemplate} from './view/header-menu.js';
+import {createFiltersTemplate} from './view/filters.js';
 
 const headerElement = document.querySelector('.page-header');
 const navigationContainerElement = headerElement.querySelector('.trip-controls__navigation');
+const filtersContainerElement = headerElement.querySelector('.trip-controls__filters');
 
 renderTemplate(navigationContainerElement, RenderPosition.BEFOREEND, createHeaderMenuTemplate());
-
+renderTemplate(filtersContainerElement, RenderPosition.BEFOREEND, createFiltersTemplate());
