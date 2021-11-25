ad1c860c3688baa621e916bcdb2dbe510adc7102
irina.lebedeva <irina.lebedeva@travelline.ru>
2021-11-25 22:46:09 +0300

    Добавляет модуль сортировки

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import {renderTemplate, RenderPosition} from './render.js';
 import {createHeaderMenuTemplate} from './view/header-menu.js';
 import {createFiltersTemplate} from './view/filters.js';
+import {createSortTemplate} from './view/sort.js';
 
 const headerElement = document.querySelector('.page-header');
 const navigationContainerElement = headerElement.querySelector('.trip-controls__navigation');
 const filtersContainerElement = headerElement.querySelector('.trip-controls__filters');
 
+const mainElement = document.querySelector('.page-main');
+const eventsContainerElement = mainElement.querySelector('.trip-events');
+
 renderTemplate(navigationContainerElement, RenderPosition.BEFOREEND, createHeaderMenuTemplate());
 renderTemplate(filtersContainerElement, RenderPosition.BEFOREEND, createFiltersTemplate());
+renderTemplate(eventsContainerElement, RenderPosition.BEFOREEND, createSortTemplate());
