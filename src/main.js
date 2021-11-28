47b75eb294a11dd747d44118864b69b67e5c02f7
irina.lebedeva <irina.lebedeva@travelline.ru>
2021-11-29 00:37:48 +0300

    Добавляет отображение данных в шаблоне точки маршрута

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import {createPointsListTemplate} from './view/points-list.js';
 import {createPointsListItemTemplate} from './view/points-list-item.js';
 import {createPointTemplate} from './view/point.js';
 import {createEditPointTemplate} from './view/edit-point.js';
+import {generatePoint} from './mock/point.js';
 
-const POINTS_COUNT = 3;
+const POINTS_COUNT = 15;
+
+const points = Array(POINTS_COUNT).fill(null).map((_, index) => generatePoint(index + 1));
 
 const headerElement = document.querySelector('.page-header');
 const navigationContainerElement = headerElement.querySelector('.trip-controls__navigation');
@@ -24,6 +27,6 @@ renderTemplate(eventsContainerElement, RenderPosition.BEFOREEND, createPointsLis
 const eventsListElement = eventsContainerElement.querySelector('.trip-events__list');
 
 renderTemplate(eventsListElement, RenderPosition.BEFOREEND, createPointsListItemTemplate(createEditPointTemplate()));
-for (let i = 0; i < POINTS_COUNT; i++) {
-  renderTemplate(eventsListElement, RenderPosition.BEFOREEND, createPointsListItemTemplate(createPointTemplate()));
+for (let i = 1; i < POINTS_COUNT; i++) {
+  renderTemplate(eventsListElement, RenderPosition.BEFOREEND, createPointsListItemTemplate(createPointTemplate(points[i])));
 }
