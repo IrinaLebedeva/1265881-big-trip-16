f6122faafb74b17e707b1c4068f7bc6a34d28b3a
irina.lebedeva <irina.lebedeva@travelline.ru>
2021-12-04 20:32:28 +0300

    Добавляет абстрактный класс

---
+++ src/view/abstract-view.js
@@ -0,0 +1,28 @@
+import {createElement} from '../render.js';
+
+class AbstractView {
+  #element = null;
+
+  constructor() {
+    if (new.target === AbstractView) {
+      throw new Error('Can\'t instantiate an Abstract class');
+    }
+  }
+
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+    return this.#element;
+  }
+
+  get template() {
+    throw new Error('Get template method is not implemented in an Abstract class');
+  }
+
+  removeElement() {
+    this.#element = null;
+  }
+}
+
+export {AbstractView};
