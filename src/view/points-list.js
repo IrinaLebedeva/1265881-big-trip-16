ff072e951d3a5fe4df517796aeedaf792f0d8ccc
irina.lebedeva <irina.lebedeva@travelline.ru>
2022-01-14 23:09:20 +0300

    Рефакторинг

--- src/view/points-list.js
+++ src/view/points-list.js
@@ -1,26 +1,16 @@
 import {AbstractView} from './abstract-view.js';
 
-const createPointsListTemplate = (pointsListClass) => (
-  `<ul class="${pointsListClass}">
+const createPointsListTemplate = () => (
+  `<ul class="trip-events__list">
   </ul>`
 );
 
 class PointsList extends AbstractView {
-  #POINTS_LIST_CLASS = 'trip-events__list';
-
   /**
    * @return {String}
    */
   get template() {
-    return createPointsListTemplate(this.pointsListClass());
-  }
-
-  pointsListClass() {
-    return this.#POINTS_LIST_CLASS;
-  }
-
-  get pointsListSelector() {
-    return `.${this.pointsListClass()}`;
+    return createPointsListTemplate();
   }
 }
 
